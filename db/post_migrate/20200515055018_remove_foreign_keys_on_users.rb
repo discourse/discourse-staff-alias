0ebb9c446440b8b7f6e66ab757e2d936af7c5876
Guo Xiang Tan <gxtan1990@gmail.com>
2020-05-15 14:28:14 +0800

    Try to avoid `ActiveRecord::Migration#change`.
    Be clear about how rollback should happen.

--- db/post_migrate/20200515055018_remove_foreign_keys_on_users.rb
+++ db/post_migrate/20200515055018_remove_foreign_keys_on_users.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class RemoveForeignKeysOnUsers < ActiveRecord::Migration[6.0]
-  def change
+  def up
     remove_foreign_key :discourse_staff_alias_users_posts_links, :users, column: :user_id
     remove_foreign_key :discourse_staff_alias_users_post_revisions_links, :users, column: :user_id
+  end
+
+  def down
+    add_foreign_key :discourse_staff_alias_users_posts_links, :users, column: :user_id
+    add_foreign_key :discourse_staff_alias_users_post_revisions_links, :users, column: :user_id
   end
 end
