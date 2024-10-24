3cc5be39b19a4edc7354134fec09a2a7f03da3c1
Gabriel Grubba <gabriel@discourse.org>
2024-10-24 17:10:02 -0300

    DEV: Update plugin annotations

--- app/models/discourse_staff_alias/users_posts_link.rb
+++ app/models/discourse_staff_alias/users_posts_link.rb
@@ -7,3 +7,22 @@ class DiscourseStaffAlias::UsersPostsLink < ActiveRecord::Base
   validates :user_id, presence: true, uniqueness: { scope: [:post_id] }
   validates :post_id, presence: true
 end
+
+# == Schema Information
+#
+# Table name: discourse_staff_alias_users_posts_links
+#
+#  id         :bigint           not null, primary key
+#  user_id    :bigint           not null
+#  post_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  idx_user_id_post_id  (user_id,post_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#
