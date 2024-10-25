cbfe39c71dd3c9c202409c9225ddaa5d9d00e0ce
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-10-25 05:49:00 -0300

    DEV: Update plugin annotations (#79)

--- app/models/discourse_staff_alias/users_post_revisions_link.rb
+++ app/models/discourse_staff_alias/users_post_revisions_link.rb
@@ -7,3 +7,22 @@ class DiscourseStaffAlias::UsersPostRevisionsLink < ActiveRecord::Base
   validates :user_id, presence: true, uniqueness: { scope: [:post_revision_id] }
   validates :post_revision_id, presence: true
 end
+
+# == Schema Information
+#
+# Table name: discourse_staff_alias_users_post_revisions_links
+#
+#  id               :bigint           not null, primary key
+#  user_id          :bigint           not null
+#  post_revision_id :bigint           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  idx_user_id_post_revision_id  (user_id,post_revision_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_revision_id => post_revisions.id)
+#
